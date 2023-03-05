c6b5b801e349ff6f13bd1d35ff0ec1c190de3bf0
Quany <quany@live.com>
2023-03-06 00:21:09 +0800

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -11,13 +11,10 @@ import { useEffect } from 'react'
  */
 const NoFound = props => {
   // todo:查Redis有没有,转发链接
-
   useEffect(() => {
     window.location.href = `https://home.l0l.ink${window.location.pathname}`;
   });
-  
-  return (<></>);
-
+  return (<div>not Found<div/>);
   // const { theme, siteInfo } = useGlobal()
   // const ThemeComponents = ThemeMap[theme]
   // const meta = { title: `${props?.siteInfo?.title} | 页面找不到啦`, image: siteInfo?.pageCover }
