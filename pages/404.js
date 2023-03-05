f2ce3f83def7ecd65e39057335bac0cc6bcca418
Quany <quany@live.com>
2023-03-06 00:14:10 +0800

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -2,16 +2,26 @@ import { getGlobalNotionData } from '@/lib/notion/getNotionData'
 import * as ThemeMap from '@/themes'
 import { useGlobal } from '@/lib/global'
 
+import { useEffect } from 'react'
+
 /**
  * 404
  * @param {*} props
  * @returns
  */
 const NoFound = props => {
-  const { theme, siteInfo } = useGlobal()
-  const ThemeComponents = ThemeMap[theme]
-  const meta = { title: `${props?.siteInfo?.title} | 页面找不到啦`, image: siteInfo?.pageCover }
-  return <ThemeComponents.Layout404 {...props} meta={meta}/>
+  // todo:查Redis有没有,转发链接
+
+  useEffect(() => {
+   window.location.href=`https://home.l0l.ink${window.location.pathname}`
+  })
+  
+  return (<></>);
+
+  // const { theme, siteInfo } = useGlobal()
+  // const ThemeComponents = ThemeMap[theme]
+  // const meta = { title: `${props?.siteInfo?.title} | 页面找不到啦`, image: siteInfo?.pageCover }
+  // return <ThemeComponents.Layout404 {...props} meta={meta}/>
 }
 
 export async function getStaticProps () {
