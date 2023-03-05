2fc59b61d5308f3414728f213b392f6cf8ca49f4
Quany <quany@live.com>
2023-03-06 00:39:44 +0800

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,27 @@
-import { getGlobalNotionData } from '@/lib/notion/getNotionData'
+// import { getGlobalNotionData } from '@/lib/notion/getNotionData'
 // import * as ThemeMap from '@/themes'
 // import { useGlobal } from '@/lib/global'
+
+// /**
+//  * 404
+//  * @param {*} props
+//  * @returns
+//  */
+// const NoFound = props => {
+//   const { theme, siteInfo } = useGlobal()
+//   const ThemeComponents = ThemeMap[theme]
+//   const meta = { title: `${props?.siteInfo?.title} | 页面找不到啦`, image: siteInfo?.pageCover }
+//   return <ThemeComponents.Layout404 {...props} meta={meta}/>
+// }
+
+// export async function getStaticProps () {
+//   const props = (await getGlobalNotionData({ from: '404' })) || {}
+//   return { props }
+// }
+
+// export default NoFound
+
+import { getGlobalNotionData } from '@/lib/notion/getNotionData'
 import { useEffect } from 'react'
 
 /**
@@ -11,12 +32,12 @@ import { useEffect } from 'react'
 const NoFound = props => {
   // todo:查Redis有没有,转发链接
   useEffect(() => {
-    window.location.href = `https://home.l0l.ink${window.location.pathname}`
+    window.location.href = 'https://home.l0l.ink' + window.location.pathname
   })
-  return (<div>not Found<div/>)
+  return <div />
 }
 
-export async function getStaticProps () {
+export async function getStaticProps() {
   const props = (await getGlobalNotionData({ from: '404' })) || {}
   return { props }
 }
